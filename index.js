0aafd82211ad97e068211def67fb764689200554
Greg Lucas <magellanarts@gmail.com>
2019-08-29 23:11:11 -0700

    trying to connect to postgres on heroku

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const app = express();
+const { Client } = require('pg');
+
+// Postgres
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+client.connect();
+
+/* client.query('SELECT NOW()', (err, res) => {
+  if (err) console.log(err);
+  // console.log(err, res);
+  client.end();
+}); */
 
 require('dotenv').config();
 
