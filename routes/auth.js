09559a8f1bb33322ce3f99f0670e4a23a6f222f2
Greg Lucas <magellanarts@gmail.com>
2019-09-03 22:22:28 -0700

    switch from client to pool

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-const { Client } = require('pg');
+const { Pool } = require('pg');
 const uuidv4 = require('uuid/v4');
 
 const { registrationValidation, loginValidation } = require('../validation');
 
-const client = new Client({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
@@ -17,13 +17,13 @@ router.post('/register', async (req, res) => {
     return res.json(error.details[0]);
   }
 
-  await client.connect();
+  await pool.connect();
 
   // Check if user exists by this email first
   const existingText = 'SELECT exists(select email FROM users WHERE email = $1)';
   const existingValue = [req.body.email];
 
-  const emailExists = await client.query(existingText, existingValue);
+  const emailExists = await pool.query(existingText, existingValue);
   if (emailExists) return res.status(400).send('User already exists');
 
   // User doesn't exist, so create them
@@ -37,7 +37,7 @@ router.post('/register', async (req, res) => {
     'INSERT INTO users(firstName, lastName, email, password, id) VALUES($1, $2, $3, $4, $5) RETURNING *';
   const addValues = [req.body.firstName, req.body.lastName, req.body.email, hashedPassword, uid];
 
-  client.query(addText, addValues, (err, result) => {
+  pool.query(addText, addValues, (err, result) => {
     if (err) {
       return res.status(400).send(err.stack);
     } else {
@@ -53,20 +53,20 @@ router.post('/login', async (req, res) => {
   if (error) return res.status(400).json(error.details);
 
   // Check if user is already in database
-  await client.connect();
+  await pool.connect();
   console.log('after connect');
 
   // Check if user exists by this email first
   const existingText = 'SELECT * FROM users WHERE email = $1';
   const existingValue = [req.body.email];
 
-  const user = await client.query(existingText, existingValue);
+  const user = await pool.query(existingText, existingValue);
   console.log('after user');
   console.log(user);
-  if (!user) return res.status(400).send('Email or password is wrong');
+  if (!user.rows[0]) return res.status(400).send('Email or password is wrong');
 
-  client.end();
-  return res.status(400).json(user);
+  pool.end();
+  return res.json(user);
 });
 
 module.exports = router;
